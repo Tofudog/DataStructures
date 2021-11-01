5c37c5bd68bf36f36e8bd6f724e5f69eb1677029
100161792 <leodefarias25@gmail.com>
2021-11-01 08:59:00 -0500

    Adding correct files to illustrate src

---
+++ src/Stacks.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Stacks {
+
+    private Stack<String> myStack;
+
+    public Stacks(Stack<String> stack) {
+        this.myStack = stack;
+    }
+
+    public void store() {
+
+    }
+
+    public static void main(String[] args) {
+        /*
+        Driver code:
+        Stack<String> pez = new Stack<>();
+        String colors = "blue red red yellow red"
+        pez.store(colors);
+        pez.dispense(int nWant);
+         */
+    }
+
+
+}
